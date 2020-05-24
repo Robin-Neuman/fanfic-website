26e706fdd88b4bac11821d38678aa0d9c223b160
Robin-Neuman <robinneuman@hotmail.com>
2020-05-25 01:47:57 +0200

    made small changes

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -15,7 +15,7 @@ export default class Home extends React.Component {
             <p>Welcome to the home of the fanfic: "I'm in love with a monster with two heads"!
             This is a newly started website by me (boyfriendo) and Pandis in the hopes of improving
             my skills in web-development, as well as helping the community grow for this glorious fanfiction!
-                            </p>
+            </p>
           </div>
           <News
             users={this.props.users}
