a65ee59374b5f86cb2546367ed8f92bb5a2294a6
Robin-Neuman <robinneuman@hotmail.com>
2020-09-09 23:56:00 +0200

    added styling

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,27 +4,55 @@ import Logout from './Logout'
 import { getRole } from '../helpers'
 
 export default class Navbar extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            navOpen: false
+        }
+    }
     render() {
+        const navOpen = this.state.navOpen
         return (
-            <div className="navBar">
-                <img src=""></img>
-                <h3 className="navBarHeader">Pandis Writes</h3>
-                <Link className="navBarItem" to={"/"}>Home</Link>
-                <Link className="navBarItem" to={"/fanfics"}>Fanfics</Link>
-                <Link className="navBarItem" to={"/forum"}>Forum</Link>
-                <Link className="navBarItem" to={"/gallery"}>Gallery</Link>
-                {this.props.loggedIn ? (
-                    <div/>
+            <div>
+                <div className="navBar">
+                    <img src=""></img>
+                    <h3 className="navBarHeader">Pandis Writes</h3>
+                    <div className="links">
+                        <Link className="navBarItem" to={"/"}>Home</Link>
+                        <Link className="navBarItem" to={"/fanfics"}>Fanfics</Link>
+                        <Link className="navBarItem" to={"/forum"}>Forum</Link>
+                        <Link className="navBarItem" to={"/gallery"}>Gallery</Link>
+                        <Link className="navBarItem" to={"/register"}>Register</Link>
+                        {this.props.loggedIn ? (
+                            <div hidden />
+                        ) : (
+                                <Link className="navBarItem" to={"/login"}>Login</Link>
+                            )}
+                        {getRole(localStorage.getItem('token')) == "admin" ? (
+                            <Link className="navBarItem" to={"/adminPage"}>Admin</Link>
+                        ) : (
+                                <div hidden />
+                            )}
+                    </div>
+                    {this.props.loggedIn ? (
+                        <div className="dropdownButton">
+                            <button onClick={() => { this.setState({ navOpen: !navOpen }) }}>Click</button>
+                        </div>
+                    ) : (
+                            <div />
+                        )}
+                </div>
+                {navOpen ? (
+                    <div className="dropdownMenu">                 
+                        <Link className="dropdownItem" to={"/"}>Profile</Link>   
+                        <Link className="dropdownItem" to={"/"}>Profile</Link>   
+                        <Link className="dropdownItem" to={"/"}>Profile</Link>   
+                        <Link className="dropdownItem" to={"/"}>Profile</Link>   
+                        <Link className="dropdownItem" to={"/"}>Profile</Link>   
+                    </div>
                 ) : (
-                    <Link className="navBarItem" to={"/login"}>Login</Link>
-                )}
-                <Link className="navBarItem" to={"/register"}>Register</Link>
-                {getRole(localStorage.getItem('token')) == "admin" ? (
-                    <Link className="navBarItem" to={"/adminPage"}>Admin</Link>
-                ) : (
-                    <div/>
-                )}
-                <Logout handleLogout={this.props.handleLogout} />
+                        <div hidden />
+                    )}
             </div>
         )
     }
